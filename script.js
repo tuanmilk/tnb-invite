c61d473eb1685f2251409ba2ec2dc5b1514a435f
tuanmilk <nguyenvutuan.tuanmilk@gmail.com>
2025-07-14 14:06:14 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 function openInvite() {
-  document.getElementById("envelope").style.display = "none";
+  document.getElementById("envelope-img").style.display = "none";
   document.getElementById("invite").style.display = "block";
   document.getElementById("bgm").play();
 }
